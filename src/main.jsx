4946218f4cc1cd8fdf754b23706ffe620c9adbda
David Mite <dav.mite0907@gmail.com>
2024-02-25 14:36:35 -0500

    Cambio en el tamaño de las imagenes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./index-responsive.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
