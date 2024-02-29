a8f4d8f5562eeeec8a2cc53164d31f35cf3b3be2
David Mite <dav.mite0907@gmail.com>
2024-02-29 16:20:40 -0500

    Cambiando carrusel de react a la libreria swiper

---
+++ src/components/CustomSwiper.jsx
@@ -0,0 +1,77 @@
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Autoplay, EffectCube, EffectCreative} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/effect-creative";
+import PropTypes from "prop-types";
+
+const CustomSwiper = ({
+  images,
+  effect = "creative",
+  grabCursor = true,
+  centeredSlides = true,
+  slidesPerView = 1,
+  direction = "horizontal",
+  loop = true,
+  creativeEffectOptions = {
+    prev: {
+      shadow: true,
+      translate: ["-120%", 0, -500],
+    },
+    next: {
+      shadow: true,
+      translate: ["120%", 0, -500],
+    },
+  },
+  cubeEffectOptions = {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+  autoplayOptions = {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  speed = 1500,
+}) => {
+  return (
+    <div className='carousel-container'>
+      <Swiper
+        effect={effect}
+        grabCursor={grabCursor}
+        centeredSlides={centeredSlides}
+        slidesPerView={slidesPerView}
+        direction={direction}
+        loop={loop}
+        creativeEffect={creativeEffectOptions}
+        cubeEffect={cubeEffectOptions}
+        autoplay={autoplayOptions}
+        speed={speed}
+        modules={[Autoplay, EffectCube, EffectCreative]}
+        className='swiper_container'>
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <img src={image} alt={`imagen-${index}`} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+CustomSwiper.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  effect: PropTypes.string,
+  grabCursor: PropTypes.bool,
+  centeredSlides: PropTypes.bool,
+  slidesPerView: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  direction: PropTypes.oneOf(["horizontal", "vertical"]),
+  loop: PropTypes.bool,
+  creativeEffectOptions: PropTypes.object,
+  cubeEffectOptions: PropTypes.object,
+  autoplayOptions: PropTypes.object,
+  speed: PropTypes.number,
+};
+
+export default CustomSwiper;
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,30 +1,19 @@
-import {Carousel} from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import data from "../../productosdest.json";
+import CustomSwiper from "../CustomSwiper.jsx";
 import ImagePortafolio from "../Productos/ImagePortafolio.jsx";
 
 const Home = () => {
   const {items} = data;
-
-  const images = ["img/fondo_imagen.webp", "img/nosotros_final.webp"];
+  const images = [
+    "img/fondo_imagen.webp",
+    "img/fondo_imagen.webp",
+    "img/fondo_imagen.webp",
+  ];
 
   return (
     <section className='hero' id='inicio'>
       <div className='carousel-container'>
-        <Carousel
-          autoPlay
-          infiniteLoop
-          showArrows={false}
-          interval={1500}
-          showStatus={false}
-          showThumbs={false}
-          className='fullscreen-carousel'>
-          {images.map((image, index) => (
-            <div key={index} className='carousel-image-container'>
-              <img src={image} alt='imagen' className='carousel-image' />
-            </div>
-          ))}
-        </Carousel>
+        <CustomSwiper images={images} effect='creative' />
       </div>
       <div className='contenido-hero'>
         <div className='textos-hero'>
