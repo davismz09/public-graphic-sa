0229e428f13ea8aef5fca79a643b5109480c8758
David Mite <dav.mite0907@gmail.com>
2024-03-09 01:14:14 -0500

    elementos

--- src/components/Menu/Capa.jsx
+++
@@ -1,8 +0,0 @@
-export default function Capa({isMenuOpen, closeMenuOnClick}) {
-  return (
-    <div
-      id='capaOpacity'
-      className={`capa--opacity ${isMenuOpen ? "active" : ""}`}
-      onClick={closeMenuOnClick}></div>
-  );
-}
