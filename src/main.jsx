97df4b5f78c176b6fb1f72f94c74771e963424a8
David Mite <dav.mite0907@gmail.com>
2024-04-03 19:01:00 -0500

    15-colores-css-a-variables

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "./index-responsive.css";
+import "./fonts.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
