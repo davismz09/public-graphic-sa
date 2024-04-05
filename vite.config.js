580a0ca54e24d78cfc344906980bc58d65bde601
David Mite <dav.mite0907@gmail.com>
2024-04-05 10:36:14 -0500

    Cambio en el nombre del proyecto

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Public-Graphic-SA/",
+  base: "/public-graphic-sa/",
 });
